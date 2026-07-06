91a9d126c7dec958a2b2c59c0d7658639b0e4c9c
Rohan Singh Rawat <110449973+RohanAnalytics@users.noreply.github.com>
2026-07-06 13:57:16 +0530

    Add files via upload

---
+++ app/config/constants.py
@@ -0,0 +1,14 @@
+"""
+Application Constants
+"""
+
+APP_VERSION = "1.0.0"
+
+PROJECT_NAME = "Learning Roadmap Concierge AI"
+
+DEFAULT_MODEL = "gemini-2.5-flash"
+
+MAX_HISTORY = 10
+
+DEFAULT_USER = "Rohan"
+
---
+++ app/config/settings.py
@@ -0,0 +1,33 @@
+"""
+Application Settings
+"""
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Settings:
+
+    APP_NAME = os.getenv(
+        "APP_NAME",
+        "Learning Roadmap Concierge AI"
+    )
+
+    MODEL_NAME = os.getenv(
+        "MODEL_NAME",
+        "gemini-2.5-flash"
+    )
+
+    GOOGLE_API_KEY = os.getenv(
+        "GOOGLE_API_KEY"
+    )
+
+    DEBUG = os.getenv(
+        "DEBUG",
+        "True"
+    )
+
+
+settings = Settings()
